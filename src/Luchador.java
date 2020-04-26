c7533505c9cab7a56dd2d110bf908e9857437e59
Luz <luz_laura_perez_nina@hotmail.com>
2020-04-25 22:56:33 -0300

    -LeerArchivo
    -atributos de Luchador agregados
    -leer archivo sumo.in
    -JUnit library añadido

--- src/Luchador.java
+++ src/Luchador.java
@@ -1,4 +1,30 @@
 
 public class Luchador {
+	private int id; //según aparición del luchador desde el archivo
+	private int peso;
+	private int altura;
+	
+	public Luchador(int id, int peso, int altura) {
+		super();
+		this.id = id;
+		this.peso = peso;
+		this.altura = altura;
+	}
 
+	public int getPeso() {
+		return peso;
+	}
+
+	public void setPeso(int peso) {
+		this.peso = peso;
+	}
+
+	public int getAltura() {
+		return altura;
+	}
+
+	public void setAltura(int altura) {
+		this.altura = altura;
+	}
+	
 }
---
+++ src/pruebas/SumoTest.java
@@ -0,0 +1,14 @@
+package pruebas;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SumoTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
